24d0fefcf051060b702242085487ccdf5d318e72
y444 <541m0nk1n9@gmail.com>
2019-02-04 23:23:16 +0300

    functional timer, functional bubble limits

---
+++ Assets/Scripts/bubblecounter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bubblecounter : MonoBehaviour
+{
+    public int maxBubbles;
+    public GameObject bubbleMarkerPrefab;
+    public int activeBubbles;
+    public GameObject[] bubbleMarkers;
+    public int bubblesLeft;
+    public float markerDownscale;
+    public Sprite normalSprite;
+    public Sprite usedSprite;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bubbleMarkers = new GameObject[maxBubbles];
+
+        for (int i = 0; i < maxBubbles; i++ )
+        {
+           bubbleMarkers[i] = Instantiate(bubbleMarkerPrefab);
+           bubbleMarkers[i].transform.SetParent(this.transform, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        activeBubbles = GameObject.FindGameObjectsWithTag("bubble").Length;
+        bubblesLeft = maxBubbles - activeBubbles;
+        for (int i = 0; i < maxBubbles; i++)
+        {
+            if (i < activeBubbles)
+            {
+                bubbleMarkers[i].transform.localScale = new Vector3(markerDownscale,markerDownscale,1);
+                bubbleMarkers[i].GetComponent<Image>().sprite = usedSprite;
+            }
+            else
+            {
+                bubbleMarkers[i].transform.localScale = new Vector3(1,1,1);
+                bubbleMarkers[i].GetComponent<Image>().sprite = normalSprite;
+            }
+        }
+
+    }
+}
--- Assets/Scripts/touchzone.cs
+++ Assets/Scripts/touchzone.cs
@@ -7,6 +7,7 @@ public class touchzone : MonoBehaviour
     
     public GameObject bubblePrefab;
     public GameObject bubbleHolder;
+    public bubblecounter bubbleCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +23,12 @@ public class touchzone : MonoBehaviour
 
     void OnMouseDown()
     {
+        if (bubbleCounter.bubblesLeft > 0)
+        {
         GameObject newBubble = Instantiate(bubblePrefab);
         newBubble.transform.parent = bubbleHolder.transform;
         newBubble.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0);
         newBubble.GetComponent<bubble>().randomIndex = UnityEngine.Random.Range(0,100);
+        }   
     }
 }
